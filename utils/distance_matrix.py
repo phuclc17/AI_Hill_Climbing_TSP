358d0ee4b81f2f7dcf161aff154be7bd27ddf871
phuclc17 <phucle172005@gmail.com>
2025-11-23 02:54:19 +0700

    sub

--- utils/distance_matrix.py
+++ utils/distance_matrix.py
@@ -47,9 +47,7 @@ class DistanceMatrix:
         dlat = lat2 - lat1
         dlon = lon2 - lon1
         
-        a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
-        c = 2 * math.asin(math.sqrt(a))
-        
+  
         earth_radius = 6371.0
         
         return earth_radius * c
